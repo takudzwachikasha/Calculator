de09b2b3ff039bb68d9a6c3b50a5fde19a240828
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-06-10 21:37:55 +0200

    Parsing Body Parser

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
