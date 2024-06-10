6f050da256beeb21e789a175688057611a62353b
Takudzwa Chikasha <harrisonchikks@gmail.com>
2024-06-10 21:31:52 +0200

    adding body parser

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    res.send("Thanks for posting that!");
 })
 
 app.listen(3000, function(){
